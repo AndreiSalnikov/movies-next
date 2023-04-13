649ba76e1eb7f8d4f408a2c5560b80b3180eef86
AndreiSalnikov <Ans762@mail.ru>
2023-04-13 13:43:32 +0300

    fix: убрал картинку из паблика, чтобы работала сборка на гитхабе

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,17 +2,14 @@ import React from 'react';
 import styles from "../Logo/Logo.module.scss";
 import Link from "next/link";
 import {useRouter} from "next/router";
-// import {Link, useLocation} from "react-router-dom";
 
 
 const Logo = () => {
   const location = useRouter()
-  // const location = useLocation();
   return (
     <Link
       className={location.pathname === '/registration' || location.pathname === '/login' ? `${styles.logo} ${styles.logo__type_center}` : `${styles.logo}`}
       href="/">
-
     </Link>
   );
 };
